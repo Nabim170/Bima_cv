552bdb08b51a0d47c64a7a422480b64554d1c298
Nabim170 <Nandabimapratama12@gmail.com>
2024-09-26 00:11:29 +0700

    Update skills.vue

--- pages/skills.vue
+++ pages/skills.vue
@@ -2,8 +2,6 @@
     <div class="min-h-screen bg-gray-100 p-6">
       <h1 class="text-3xl font-bold mb-4">Keahlian</h1>
       <ul class="list-disc list-inside bg-white p-4 rounded-lg shadow">
-        <li>Bisa meracik obat (Beginner)</li>
-        <li>Bisa mengemudi mobil (Upper Intermediate)</li>
         <li>Microsoft Office (Expert)</li>
         <li>Desain website UI/UX dan Figma (Intermediate)</li>
         <li>Bahasa Inggris (Intermediate)</li>
