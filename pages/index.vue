ef76491173cc3bf09edcad2dab1c08df4cc3fc4f
Nabim170 <Nandabimapratama12@gmail.com>
2024-09-26 00:10:54 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <img src="/img-1.jpeg" alt="Nanda Bima Pratama" class="rounded-full w-40 mx-auto mb-4" />
       <h1 class="text-4xl font-bold">Nanda Bima Pratama</h1>
       <p class="text-gray-600 mt-2">
-        Lulusan baru di bidang Teknologi Informasi dengan minat besar dalam pengembangan web.
+        Mahasiswa baru di bidang Teknologi Informasi dengan minat besar dalam pengembangan web.
       </p>
       <div class="mt-4">
         <a href="https://linkedin.com/in/nanda-bima-08aa8" target="_blank" class="text-blue-600">LinkedIn</a> | 
